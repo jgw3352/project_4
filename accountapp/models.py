74d2fa2d945307d4df096c9e22157e268034aebc
jgw3352 <jgw0023@likelion.org>
2021-07-09 12:59:16 +0900

    project4 7/9 - 2

--- accountapp/models.py
+++ accountapp/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+
+# Create your models here.
+
+
+
+class NewModel(models.Model):
+    text = models.CharField(max_length=255, null=False)
+
+#makemigrations = model변경을 감지하고 변경사항을 반영할 파일 생성
+
+
